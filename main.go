34601f52d297a215925942e6a6c994067ffb08ff
zaxwebs <zifidigital@gmail.com>
2025-04-08 14:24:25 +0530

    added time route

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +23,15 @@ func handlerX(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello from Go - X!")
 }
 
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/time" {
+		notFoundHandler(w, r)
+		return
+	}
+	currentTime := time.Now().Format(time.RFC1123)
+	fmt.Fprintf(w, "Current time: %s", currentTime)
+}
+
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprintln(w, "Custom 404 - Page not found.")
@@ -30,6 +40,7 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/x", handlerX)
+	http.HandleFunc("/time", timeHandler)
 
 	fmt.Println("Starting server at :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
